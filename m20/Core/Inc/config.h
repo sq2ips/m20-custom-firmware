b076e8454c0f4e811f229494d0938d188cc0c2c2
sq2ips <pawel.sq2ips@gmail.com>
2026-01-14 22:30:11 +0100

    Horus V3 payload call note in config

--- m20/Core/Inc/config.h
+++ m20/Core/Inc/config.h
@@ -18,7 +18,7 @@ const static float QRG_FSK4[] = {
 
 #define HORUS_V2_PAYLOAD_ID 256 // Sonde payload ID 256 - for 4FSKTEST-V2, change this for real flight, refer to https://github.com/projecthorus/horusdemodlib/wiki#how-do-i-transmit-it
 
-#define HORUS_V3_PAYLOAD_CALLSIGN "4FSKTEST-V3"
+#define HORUS_V3_PAYLOAD_CALLSIGN "4FSKTEST-V3" // Sonde payload callsing, 1 to 15 characters : a-z,A-Z,0-9,-, note that the longer it is, the longer the frame will be.
 
 #define TX_PAUSE 500 // Delay between HORUS and APRS
 
