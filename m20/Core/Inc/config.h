17b30942fafeb863af8755e4f7b7577057249879
sq2ips <pawel.sq2ips@gmail.com>
2026-03-26 08:11:26 +0100

    Default same power for both modulations

--- m20/Core/Inc/config.h
+++ m20/Core/Inc/config.h
@@ -29,7 +29,7 @@ const static float QRG_FSK4[] = {
 
 const static float QRG_AFSK[] = {
     435100000};       // AFSK transmitted frequencies array, switched in a loop, add new frequencies (in Hz) after a comma in braces.
-#define AFSK_POWER 10 // RF power setting AFSK transmission values 0-63
+#define AFSK_POWER FSK4_POWER // RF power setting AFSK transmission values 0-63
 
 #define APRS_CALLSIGN "NOCALL" // Sonde callsign, max 6 digits, change this for your callsign for real flight
 #define APRS_SSID 11           // Sonde SSID, 11 is "balloons, aircraft, spacecraft, etc", refer to https://www.aprs.org/aprs11/SSIDs.txt
